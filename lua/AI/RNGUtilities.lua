ddc746913c9e4d2b30564e5641e937b9f02716f7
Aaron Warner <aaron.warner@revera.co.nz>
2019-12-06 15:01:49 +1300

    add skeleton of scoutmasslocations

--- lua/AI/RNGUtilities.lua
+++ lua/AI/RNGUtilities.lua
@@ -441,3 +441,120 @@ end
 function PositionInWater(pos)
 	return GetTerrainHeight(pos[1], pos[3]) < GetSurfaceHeight(pos[1], pos[3])
 end
+
+BuildMassScoutLocations = function(aiBrain)
+    local aiBrain = self
+    local opponentStarts = {}
+    local allyStarts = {}
+
+    if not aiBrain.InterestList then
+        aiBrain.InterestList = {}
+        aiBrain.IntelData.HiPriScouts = 0
+        aiBrain.IntelData.AirHiPriScouts = 0
+        aiBrain.IntelData.AirLowPriScouts = 0
+
+        -- Add each enemy's start location to the InterestList as a new sub table
+        aiBrain.InterestList.HighPriority = {}
+        aiBrain.InterestList.LowPriority = {}
+        aiBrain.InterestList.MustScout = {}
+
+        local myArmy = ScenarioInfo.ArmySetup[self.Name]
+
+        if ScenarioInfo.Options.TeamSpawn == 'fixed' then
+            -- Spawn locations were fixed. We know exactly where our opponents are.
+            -- Don't scout areas owned by us or our allies.
+            local numOpponents = 0
+            for i = 1, 16 do
+                local army = ScenarioInfo.ArmySetup['ARMY_' .. i]
+                local startPos = ScenarioUtils.GetMarker('ARMY_' .. i).position
+                if army and startPos then
+                    if army.ArmyIndex ~= myArmy.ArmyIndex and (army.Team ~= myArmy.Team or army.Team == 1) then
+                    -- Add the army start location to the list of interesting spots.
+                    opponentStarts['ARMY_' .. i] = startPos
+                    numOpponents = numOpponents + 1
+                    table.insert(aiBrain.InterestList.HighPriority,
+                        {
+                            Position = startPos,
+                            LastScouted = 0,
+                        }
+                    )
+                    else
+                        allyStarts['ARMY_' .. i] = startPos
+                    end
+                end
+            end
+
+            aiBrain.NumOpponents = numOpponents
+
+            -- For each vacant starting location, check if it is closer to allied or enemy start locations (within 100 ogrids)
+            -- If it is closer to enemy territory, flag it as high priority to scout.
+            local starts = AIUtils.AIGetMarkerLocations(aiBrain, 'Start Location')
+            for _, loc in starts do
+                -- If vacant
+                if not opponentStarts[loc.Name] and not allyStarts[loc.Name] then
+                    local closestDistSq = 999999999
+                    local closeToEnemy = false
+
+                    for _, pos in opponentStarts do
+                        local distSq = VDist2Sq(pos[1], pos[3], loc.Position[1], loc.Position[3])
+                        -- Make sure to scout for bases that are near equidistant by giving the enemies 100 ogrids
+                        if distSq-10000 < closestDistSq then
+                            closestDistSq = distSq-10000
+                            closeToEnemy = true
+                        end
+                    end
+
+                    for _, pos in allyStarts do
+                        local distSq = VDist2Sq(pos[1], pos[3], loc.Position[1], loc.Position[3])
+                        if distSq < closestDistSq then
+                            closestDistSq = distSq
+                            closeToEnemy = false
+                            break
+                        end
+                    end
+
+                    if closeToEnemy then
+                        table.insert(aiBrain.InterestList.LowPriority,
+                            {
+                                Position = loc.Position,
+                                LastScouted = 0,
+                            }
+                        )
+                    end
+                end
+            end
+
+        else -- Spawn locations were random. We don't know where our opponents are. Add all non-ally start locations to the scout list
+            local numOpponents = 0
+            for i = 1, 16 do
+                local army = ScenarioInfo.ArmySetup['ARMY_' .. i]
+                local startPos = ScenarioUtils.GetMarker('ARMY_' .. i).position
+
+                if army and startPos then
+                    if army.ArmyIndex == myArmy.ArmyIndex or (army.Team == myArmy.Team and army.Team ~= 1) then
+                        allyStarts['ARMY_' .. i] = startPos
+                    else
+                        numOpponents = numOpponents + 1
+                    end
+                end
+            end
+
+            aiBrain.NumOpponents = numOpponents
+
+            -- If the start location is not ours or an ally's, it is suspicious
+            local starts = AIUtils.AIGetMarkerLocations(aiBrain, 'Start Location')
+            for _, loc in starts do
+                -- If vacant
+                if not allyStarts[loc.Name] then
+                    table.insert(aiBrain.InterestList.LowPriority,
+                        {
+                            Position = loc.Position,
+                            LastScouted = 0,
+                        }
+                    )
+                end
+            end
+        end
+        aiBrain:ForkThread(self.ParseIntelThread)
+    end
+end,
