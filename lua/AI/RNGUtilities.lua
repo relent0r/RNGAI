f0e343c17eff1f4b9825f9282effb911870483f9
Aaron Warner <aaron.warner@revera.co.nz>
2019-12-06 16:02:32 +1300

    define start locations for mass marker scouting

--- lua/AI/RNGUtilities.lua
+++ lua/AI/RNGUtilities.lua
@@ -442,10 +442,11 @@ function PositionInWater(pos)
 	return GetTerrainHeight(pos[1], pos[3]) < GetSurfaceHeight(pos[1], pos[3])
 end
 
-BuildMassScoutLocations = function(aiBrain)
+BuildMassScoutLocations = function(aiBrain, includeWater)
     local aiBrain = self
     local opponentStarts = {}
     local allyStarts = {}
+    local startLocations = {}
 
     if not aiBrain.InterestList then
         aiBrain.InterestList = {}
@@ -468,59 +469,7 @@ BuildMassScoutLocations = function(aiBrain)
                 local army = ScenarioInfo.ArmySetup['ARMY_' .. i]
                 local startPos = ScenarioUtils.GetMarker('ARMY_' .. i).position
                 if army and startPos then
-                    if army.ArmyIndex ~= myArmy.ArmyIndex and (army.Team ~= myArmy.Team or army.Team == 1) then
-                    -- Add the army start location to the list of interesting spots.
-                    opponentStarts['ARMY_' .. i] = startPos
-                    numOpponents = numOpponents + 1
-                    table.insert(aiBrain.InterestList.HighPriority,
-                        {
-                            Position = startPos,
-                            LastScouted = 0,
-                        }
-                    )
-                    else
-                        allyStarts['ARMY_' .. i] = startPos
-                    end
-                end
-            end
-
-            aiBrain.NumOpponents = numOpponents
-
-            -- For each vacant starting location, check if it is closer to allied or enemy start locations (within 100 ogrids)
-            -- If it is closer to enemy territory, flag it as high priority to scout.
-            local starts = AIUtils.AIGetMarkerLocations(aiBrain, 'Start Location')
-            for _, loc in starts do
-                -- If vacant
-                if not opponentStarts[loc.Name] and not allyStarts[loc.Name] then
-                    local closestDistSq = 999999999
-                    local closeToEnemy = false
-
-                    for _, pos in opponentStarts do
-                        local distSq = VDist2Sq(pos[1], pos[3], loc.Position[1], loc.Position[3])
-                        -- Make sure to scout for bases that are near equidistant by giving the enemies 100 ogrids
-                        if distSq-10000 < closestDistSq then
-                            closestDistSq = distSq-10000
-                            closeToEnemy = true
-                        end
-                    end
-
-                    for _, pos in allyStarts do
-                        local distSq = VDist2Sq(pos[1], pos[3], loc.Position[1], loc.Position[3])
-                        if distSq < closestDistSq then
-                            closestDistSq = distSq
-                            closeToEnemy = false
-                            break
-                        end
-                    end
-
-                    if closeToEnemy then
-                        table.insert(aiBrain.InterestList.LowPriority,
-                            {
-                                Position = loc.Position,
-                                LastScouted = 0,
-                            }
-                        )
-                    end
+                    table.insert(startLocations, startPos)
                 end
             end
 
@@ -532,29 +481,14 @@ BuildMassScoutLocations = function(aiBrain)
 
                 if army and startPos then
                     if army.ArmyIndex == myArmy.ArmyIndex or (army.Team == myArmy.Team and army.Team ~= 1) then
-                        allyStarts['ARMY_' .. i] = startPos
+                        table.insert(startLocations, startPos)
                     else
-                        numOpponents = numOpponents + 1
+                        LOG('No Army Detected at start location')
                     end
                 end
             end
-
-            aiBrain.NumOpponents = numOpponents
-
-            -- If the start location is not ours or an ally's, it is suspicious
-            local starts = AIUtils.AIGetMarkerLocations(aiBrain, 'Start Location')
-            for _, loc in starts do
-                -- If vacant
-                if not allyStarts[loc.Name] then
-                    table.insert(aiBrain.InterestList.LowPriority,
-                        {
-                            Position = loc.Position,
-                            LastScouted = 0,
-                        }
-                    )
-                end
-            end
-        end
+        -- From here on we must do. See RNGAI wiki for implementation details
+        local massLocations = AIGetMassMarkerLocations(aiBrain, includeWater)
         aiBrain:ForkThread(self.ParseIntelThread)
     end
 end,
