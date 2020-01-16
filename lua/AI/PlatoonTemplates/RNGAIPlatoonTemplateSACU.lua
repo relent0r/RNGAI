4d8485bd7b305f2bc77c22d09907ec38784f6c56
Aaron Warner <aaron.warner@revera.co.nz>
2020-01-17 11:19:44 +1300

    add platoon templates for SACU formers to support the category changes in the blueprints.lua function.

--- lua/AI/PlatoonTemplates/RNGAIPlatoonTemplateSACU.lua
+++ lua/AI/PlatoonTemplates/RNGAIPlatoonTemplateSACU.lua
@@ -1,4 +1,222 @@
 
+PlatoonTemplate {
+    Name = 'RNGAI SACU RAMBO preset 12345',
+    FactionSquads = {
+        UEF = {
+            { categories.RAMBO, 1, 1, 'Attack', 'None' }
+        },
+        Aeon = {
+            { categories.RAMBO, 1, 1, 'Attack', 'None' }
+        },
+        Cybran = {
+            { categories.RAMBO, 1, 1, 'Attack', 'None' }
+        },
+        Seraphim = {
+            { categories.RAMBO, 1, 1, 'Attack', 'none' }
+        },
+        Nomads = {
+            { categories.RAMBO, 1, 1, 'Attack', 'none' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU ENGINEER preset 12345',
+    FactionSquads = {
+        UEF = {
+            { categories.ENGINEER, 1, 1, 'Attack', 'None' }
+        },
+        Aeon = {
+            { categories.ENGINEER, 1, 1, 'Attack', 'None' }
+        },
+        Cybran = {
+            { categories.ENGINEER, 1, 1, 'Attack', 'None' }
+        },
+        Seraphim = {
+            { categories.ENGINEER, 1, 1, 'Attack', 'None' }
+        },
+        Nomads = {
+            { categories.ENGINEER, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU RAS preset 123x5',
+    FactionSquads = {
+        UEF = {
+            { categories.ANTIAIR, 1, 1, 'Attack', 'None' }
+        },
+        Aeon = {
+            { categories.ANTIAIR, 1, 1, 'Attack', 'None' }
+        },
+        Cybran = {
+            { categories.ANTIAIR, 1, 1, 'Attack', 'None' }
+        },
+        Nomads = {
+            { categories.ANTIAIR, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU COMBAT preset 1x34x',
+    FactionSquads = {
+        UEF = {
+            { categories.COMBAT, 1, 1, 'Attack', 'None' }
+        },
+        Cybran = {
+            { categories.COMBAT, 1, 1, 'Attack', 'None' }
+        },
+        Seraphim = {
+            { categories.COMBAT, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU NANOCOMBAT preset x2x4x',
+    FactionSquads = {
+        Aeon = {
+            { categories.NANOCOMBAT, 1, 1, 'Attack', 'None' }
+        },
+        Seraphim = {
+            { categories.NANOCOMBAT, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU BUBBLESHIELD preset 1xxxx',
+    FactionSquads = {
+        UEF = {
+            { categories.FASTCOMBAT, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU INTELJAMMER preset 1xxxx',
+    FactionSquads = {
+        UEF = {
+            { categories.INTELJAMMER, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU SIMPLECOMBAT preset x2xxx',
+    FactionSquads = {
+        Aeon = {
+            { categories.SIMPLECOMBAT, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU SHIELDCOMBAT preset x2xxx',
+    FactionSquads = {
+        Aeon = {
+            { categories.SHIELDCOMBAT, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU ANTIAIR preset xx3xx',
+    FactionSquads = {
+        Cybran = {
+            { categories.ANTIAIR, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU STEALTH preset xx3xx',
+    FactionSquads = {
+        Cybran = {
+            { categories.STEALTH, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU CLOAK preset xx3xx',
+    FactionSquads = {
+        Cybran = {
+            { categories.CLOAK, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU MISSILE preset xxx4x',
+    FactionSquads = {
+        Seraphim = {
+            { categories.MISSILE, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU ADVANCEDCOMBAT preset xxx4x',
+    FactionSquads = {
+        Seraphim = {
+            { categories.ADVANCEDCOMBAT, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU ROCKET preset xxxx5',
+    FactionSquads = {
+        Nomads = {
+            { categories.ROCKET, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU ANTINAVAL preset xxxx5',
+    FactionSquads = {
+        Nomads = {
+            { categories.ANTINAVAL, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU AMPHIBIOUS preset xxxx5',
+    FactionSquads = {
+        Nomads = {
+            { categories.AMPHIBIOUS, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU GUNSLINGER preset  xxxx5',
+    FactionSquads = {
+        Nomads = {
+            { categories.GUNSLINGER, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU NATURALPRODUCER preset xxxx5',
+    FactionSquads = {
+        Nomads = {
+            { categories.NATURALPRODUCER, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU DEFAULT preset xxxx5',
+    FactionSquads = {
+        Nomads = {
+            { categories.DEFAULT, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU HEAVYTROOPER preset xxxx5',
+    FactionSquads = {
+        Nomads = {
+            { categories.HEAVYTROOPER, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
+PlatoonTemplate {
+    Name = 'RNGAI SACU FASTCOMBAT preset xxxx5',
+    FactionSquads = {
+        Nomads = {
+            { categories.FASTCOMBAT, 1, 1, 'Attack', 'None' }
+        },
+    }
+}
 
 PlatoonTemplate {
     Name = 'RNGAI SACU Engineer preset',
