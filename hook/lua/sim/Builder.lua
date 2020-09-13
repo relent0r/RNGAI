17e605c756c07e7895e0bd8aba5bece90c056a29
Aaron Warner <aaron.warner@revera.co.nz>
2020-09-14 11:28:45 +1200

    hook calculate priority functions to support active expansions

--- hook/lua/sim/Builder.lua
+++ hook/lua/sim/Builder.lua
@@ -1,49 +1,76 @@
 
-RNGBuilder = Builder
-Builder = Class(RNGBuilder) {
---[[
-    Create = function(self, brain, data, locationType)
+
+RNGPlatoonBuilder = PlatoonBuilder
+PlatoonBuilder = Class(RNGPlatoonBuilder) {
+
+    CalculatePriority = function(self, builderManager)
+       -- Only use this with RNG
         if not self.Brain.RNG then
-            return RNGBuilder.Create(self, brain, data, locationType)
+            return TheOldPlatoonBuilder.CalculatePriority(self, builderManager)
         end
-        -- make sure the table of strings exist, they are required for the builder
-        local verifyDictionary = { 'Priority', 'BuilderName' }
-        for k,v in verifyDictionary do
-            if not self:VerifyDataName(v, data) then return false end
-        end
-
-        self.Priority = data.Priority
-        self.OriginalPriority = self.Priority
-        LOG('Data'..repr(data))
-        self.Restriction = data.Restriction
-
-        self.Brain = brain
-
-        self.BuilderName = data.BuilderName
-        
-        self.DelayEqualBuildPlattons = data.DelayEqualBuildPlattons
-
-        self.ReportFailure = data.ReportFailure
-
-        self:SetupBuilderConditions(data, locationType)
-
-        self.BuilderStatus = false
-
-        return true
-    end,
-
-    CalculatePriorityRNG = function(self, builderManager)
         self.PriorityAltered = false
-        LOG('Calculate Priority Function, checking if PriorityFunction present for location'..builderManager.LocationType)
         if Builders[self.BuilderName].PriorityFunction then
             --LOG('Calculate new Priority '..self.BuilderName..' - '..self.Priority)
-            local newPri = Builders[self.BuilderName]:PriorityFunction(self.Brain, builderManager)
+            local newPri = Builders[self.BuilderName]:PriorityFunction(self.Brain)
             if newPri != self.Priority then
+                --LOG('* AI-RNG: PlatoonBuilder New Priority:  [[  '..self.Priority..' -> '..newPri..'  ]]  -  '..self.BuilderName..'.')
                 self.Priority = newPri
                 self.PriorityAltered = true
             end
-            --LOG('New Priority '..self.BuilderName..' - '..self.Priority)
+            --LOG('RNGPlatoonBuilder New Priority '..self.BuilderName..' - '..self.Priority)
         end
         return self.PriorityAltered
-    end,]]
+    end,
+
+}
+
+RNGFactoryBuilder = FactoryBuilder
+FactoryBuilder = Class(RNGFactoryBuilder) {
+
+
+
+    CalculatePriority = function(self, builderManager)
+       -- Only use this with RNG
+        if not self.Brain.RNG then
+            return RNGFactoryBuilder.CalculatePriority(self, builderManager)
+        end
+        self.PriorityAltered = false
+        if Builders[self.BuilderName].PriorityFunction then
+            --LOG('Calculate new Priority '..self.BuilderName..' - '..self.Priority)
+            local newPri = Builders[self.BuilderName]:PriorityFunction(self.Brain)
+            if newPri != self.Priority then
+                --LOG('* AI-RNG: FactoryBuilder New Priority:  [[  '..self.Priority..' -> '..newPri..'  ]]  -  '..self.BuilderName..'.')
+                self.Priority = newPri
+                self.PriorityAltered = true
+            end
+            --LOG('RNGFactoryBuilder New Priority '..self.BuilderName..' - '..self.Priority)
+        end
+        return self.PriorityAltered
+    end,
+
+}
+
+
+RNGEngineerBuilder = EngineerBuilder
+EngineerBuilder = Class(RNGEngineerBuilder) {
+
+    CalculatePriority = function(self, builderManager)
+       -- Only use this with AI-RNG
+        if not self.Brain.RNG then
+            return RNGEngineerBuilder.CalculatePriority(self, builderManager)
+        end
+        self.PriorityAltered = false
+        if Builders[self.BuilderName].PriorityFunction then
+            --LOG('Calculate new Priority '..self.BuilderName..' - '..self.Priority)
+            local newPri = Builders[self.BuilderName]:PriorityFunction(self.Brain)
+            if newPri != self.Priority then
+                --LOG('* AI-RNG: EngineerBuilder New Priority:  [[  '..self.Priority..' -> '..newPri..'  ]]  -  '..self.BuilderName..'.')
+                self.Priority = newPri
+                self.PriorityAltered = true
+            end
+            --LOG('RNGEngineerBuilder New Priority '..self.BuilderName..' - '..self.Priority)
+        end
+        return self.PriorityAltered
+    end,
+
 }
