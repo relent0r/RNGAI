60a220172bd55d386fa1095666d39b44f8241486
Aaron Warner <aaronpaulwarner@gmail.com>
2021-04-05 20:30:49 +1200

    release 84

--- lua/AI/AIBaseTemplates/RNGAI MainBase Standard Large Null.lua
+++ lua/AI/AIBaseTemplates/RNGAI MainBase Standard Large Null.lua
@@ -82,7 +82,7 @@ BaseBuilderTemplate {
 
         -- Strategic Builders
         'RNGAI SML Builders',
-        'RNGAI Strategic Artillery Builders',
+        'RNGAI Strategic Artillery Builders Large',
         'RNGAI Strategic Formers',
 
         -- Experimentals --
