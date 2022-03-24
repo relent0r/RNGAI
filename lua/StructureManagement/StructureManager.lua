e1f70e586453a1259481d58cdba3950c8d249f36
Aaron Warner <aaronpaulwarner@gmail.com>
2022-03-24 19:40:00 +1300

    create support upgrade tables and faction based indexes

--- lua/StructureManagement/StructureManager.lua
+++ lua/StructureManagement/StructureManager.lua
@@ -18,12 +18,22 @@ StructureManager = Class {
                 {
                     Units = {},
                     UpgradingCount = 0,
-                    HQCount = 0
+                    HQCount = {
+                        ['ueb0202'] = 0,
+                        ['uab0202'] = 0,
+                        ['urb0202'] = 0,
+                        ['xsb0202'] = 0
+                    }
                 },
                 {
                     Units = {},
                     UpgradingCount = 0,
-                    HQCount = 0
+                    HQCount = {
+                        ['ueb0302'] = 0,
+                        ['uab0302'] = 0,
+                        ['urb0302'] = 0,
+                        ['xsb0302'] = 0
+                    }
                 }
             },
             LAND = {
@@ -34,12 +44,22 @@ StructureManager = Class {
                 {
                     Units = {},
                     UpgradingCount = 0,
-                    HQCount = 0
+                    HQCount = {
+                        ['ueb0201'] = 0,
+                        ['uab0201'] = 0,
+                        ['urb0201'] = 0,
+                        ['xsb0201'] = 0
+                    }
                 },
                 {
                     Units = {},
                     UpgradingCount = 0,
-                    HQCount = 0
+                    HQCount = HQCount = {
+                        ['ueb0301'] = 0,
+                        ['uab0301'] = 0,
+                        ['urb0301'] = 0,
+                        ['xsb0301'] = 0
+                    }
                 }
             },
             NAVAL = {
@@ -50,20 +70,74 @@ StructureManager = Class {
                 {
                     Units = {},
                     UpgradingCount = 0,
-                    HQCount = 0
+                    HQCount = {
+                        ['ueb0103'] = 0,
+                        ['uab0103'] = 0,
+                        ['urb0103'] = 0,
+                        ['xsb0103'] = 0
+                    }
                 },
                 {
                     Units = {},
                     UpgradingCount = 0,
-                    HQCount = 0
+                    HQCount = {
+                        ['ueb0103'] = 0},
+                        ['uab0103'] = 0},
+                        ['urb0103'] = 0},
+                        ['xsb0103'] = 0}
+                    }
                 }
             }
+        }
+        self.SupportUpgradeTable = {
+            LAND = {
+                T2 = {
+                    {['ueb0101'] = 'zeb9501'},
+                    {['uab0101'] = 'zab9501'},
+                    {['urb0101'] = 'zrb9501'},
+                    {['xsb0101'] = 'zsb9501'}
+                },
+                T3 = {
+                    {['ueb0201'] = 'zeb9601'},
+                    {['uab0201'] = 'zab9601'},
+                    {['urb0201'] = 'zrb9601'},
+                    {['xsb0201'] = 'zsb9601'}
+                }
+            },
+            AIR = {
+                T2 = {
+                    {['ueb0102'] = 'zeb9502'},
+                    {['uab0102'] = 'zab9502'},
+                    {['urb0102'] = 'zrb9502'},
+                    {['xsb0102'] = 'zsb9502'}
+                },
+                T3 = {
+                    {['ueb0202'] = 'zeb9602'},
+                    {['uab0202'] = 'zab9602'},
+                    {['urb0202'] = 'zrb9602'},
+                    {['xsb0202'] = 'zsb9602'}
+                }
+            },
+            NAVAL = {
+                T2 = {
+                    {['ueb0103'] = 'zeb9503'},
+                    {['uab0103'] = 'zab9503'},
+                    {['urb0103'] = 'zrb9503'},
+                    {['xsb0103'] = 'zsb9503'}
+                },
+                T3 = {
+                    {['ueb0203'] = 'zeb9603'},
+                    {['uab0203'] = 'zab9603'},
+                    {['urb0203'] = 'zrb9603'},
+                    {['xsb0203'] = 'zsb9603'}
+                }
+            },
         }
     end,
 
     Run = function(self)
         LOG('RNGAI : StructureManager Starting')
-        self:ForkThread(self.ValidateFactoryUpgradeRNG)
+        self:ForkThread(self.FactoryDataCaptureRNG)
         if self.Debug then
             self:ForkThread(self.StructureDebugThread)
         end
@@ -80,16 +154,13 @@ StructureManager = Class {
         end
     end,
 
-    ValidateFactoryUpgradeRNG = function(self)
+    FactoryDataCaptureRNG = function(self)
         -- Lets try be smart about how we do this
-            -- The current conditions, we'll just do like for like for now
-            --'GreaterThanGameTimeRNG', { 450, true } },
-            --'LessThanFactoryCountRNG', { 1, categories.STRUCTURE * categories.FACTORY * categories.LAND * (categories.TECH2 + categories.TECH3) - categories.SUPPORTFACTORY } },
-            --'GreaterThanEconIncomeCombinedRNG',  { 2.5, 20.0 }},
-            --'GreaterThanEconStorageRatioRNG', { 0.03, 0.3}},
-            --'HaveLessThanUnitsInCategoryBeingUpgradedRNG', { 1, categories.STRUCTURE * categories.FACTORY * categories.LAND * categories.TECH1 }},
-            --'GreaterThanEconEfficiencyCombinedRNG', { 1.025, 1.025 }},
-
+        -- This captures the current factory states, replaces all those builder conditions
+        -- Note it uses the factory managers rather than getlistofunits
+        -- This means we won't capture factories that are potentially given in fullshare
+        -- So I might need to manage that somewhere else
+        -- Or maybe we should just use getlistofunits instead but then we won't know which base they are in.. tbd
         coroutine.yield(Random(5,20))
         local ALLBPS = __blueprints
         while true do
@@ -123,74 +194,82 @@ StructureManager = Class {
                         if not unit.Dead and not unit:BeenDestroyed() then
                             if ALLBPS[unit.UnitId].CategoriesHash.LAND then
                                 if ALLBPS[unit.UnitId].CategoriesHash.TECH1 then
-                                    RNGINSERT(self.Factories.LAND[1].Units, unit)
+                                    RNGINSERT(FactoryData.T1LAND, unit)
                                     if unit:IsUnitState('Upgrading') then
                                         FactoryData.T1LANDUpgrading = FactoryData.T1LANDUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH2 then
-                                    RNGINSERT(self.Factories.LAND[2].Units, unit)
-                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
+                                    RNGINSERT(FactoryData.T2LAND, unit)
+                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORTFACTORY then
                                         FactoryData.T2LANDHQCount = FactoryData.T2LANDHQCount + 1
                                     end
                                     if unit:IsUnitState('Upgrading') then
                                         FactoryData.T2LANDUpgrading = FactoryData.T2LANDUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH3 then
-                                    RNGINSERT(self.Factories.LAND[3].Units, unit)
-                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
+                                    RNGINSERT(FactoryData.T3LAND, unit)
+                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORTFACTORY then
                                         FactoryData.T3LANDHQCount = FactoryData.T3LANDHQCount + 1
                                     end
                                 end
                             elseif ALLBPS[unit.UnitId].CategoriesHash.AIR then
                                 if ALLBPS[unit.UnitId].CategoriesHash.TECH1 then
-                                    RNGINSERT(self.Factories.AIR[1].Units, unit)
+                                    RNGINSERT(FactoryData.T1AIR, unit)
                                     if unit:IsUnitState('Upgrading') then
                                         FactoryData.T1AIRUpgrading = FactoryData.T1AIRUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH2 then
-                                    RNGINSERT(self.Factories.AIR[2].Units, unit)
-                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
+                                    RNGINSERT(FactoryData.T2AIR, unit)
+                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORTFACTORY then
                                         FactoryData.T2AIRHQCount = FactoryData.T2AIRHQCount + 1
                                     end
                                     if unit:IsUnitState('Upgrading') then
                                         FactoryData.T2AIRUpgrading = FactoryData.T2AIRUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH3 then
-                                    RNGINSERT(self.Factories.AIR[3].Units, unit)
-                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
+                                    RNGINSERT(FactoryData.T3AIR, unit)
+                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORTFACTORY then
                                         FactoryData.T3AIRHQCount = FactoryData.T3AIRHQCount + 1
                                     end
                                 end
 
                             elseif ALLBPS[unit.UnitId].CategoriesHash.NAVAL then
                                 if ALLBPS[unit.UnitId].CategoriesHash.TECH1 then
-                                    RNGINSERT(self.Factories.NAVAL[1].Units, 1, unit)
+                                    RNGINSERT(FactoryData.T1NAVAL, 1, unit)
                                     if unit:IsUnitState('Upgrading') then
                                         FactoryData.T1NAVALUpgrading = FactoryData.T1NAVALUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH2 then
-                                    RNGINSERT(self.Factories.NAVAL[2].Units, unit)
-                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
+                                    RNGINSERT(FactoryData.T2NAVAL, unit)
+                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORTFACTORY then
                                         FactoryData.T2NAVALHQCount = FactoryData.T2NAVALHQCount + 1
                                     end
                                     if unit:IsUnitState('Upgrading') then
                                         FactoryData.T2NAVALUpgrading = FactoryData.T2NAVALUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH3 then
-                                    RNGINSERT(self.Factories.NAVAL[3].Units, unit)
-                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
+                                    RNGINSERT(FactoryData.T3NAVAL, unit)
+                                    if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORTFACTORY then
                                         FactoryData.T3NAVALHQCount = FactoryData.T3NAVALHQCount + 1
                                     end
-                                    FactoryData.T3NAVALUpgrading = FactoryData.T3NAVALUpgrading + 1
                                 end
                             end
                         end
                     end
                 end
             end
-            self.Factories.LAND[1].UpgradingCount
-            self.Factories.LAND[2].UpgradingCount
-            self.Factories.LAND[2].HQCount
+            self.Factories.LAND[1].UpgradingCount = FactoryData.T1LANDUpgrading
+            self.Factories.LAND[2].UpgradingCount = FactoryData.T2LANDUpgrading
+            self.Factories.LAND[2].HQCount = FactoryData.T2LANDHQCount
+            self.Factories.LAND[3].HQCount = FactoryData.T3LANDHQCount
+            self.Factories.AIR[1].UpgradingCount = FactoryData.T1AIRUpgrading
+            self.Factories.AIR[2].UpgradingCount = FactoryData.T2AIRUpgrading
+            self.Factories.AIR[2].HQCount = FactoryData.T2AIRHQCount
+            self.Factories.AIR[3].HQCount = FactoryData.T3AIRHQCount
+            self.Factories.NAVAL[1].UpgradingCount = FactoryData.T1NAVALUpgrading
+            self.Factories.NAVAL[2].UpgradingCount = FactoryData.T2NAVALUpgrading
+            self.Factories.NAVAL[2].HQCount = FactoryData.T2NAVALHQCount
+            self.Factories.NAVAL[3].HQCount = FactoryData.T3NAVALHQCount
             LOG('Structure Manager')
             LOG('Number of upgrading T1 Land '..self.Factories.LAND[1].UpgradingCount)
             LOG('Number of upgrading T2 Land '..self.Factories.LAND[2].UpgradingCount)
@@ -204,8 +283,56 @@ StructureManager = Class {
             LOG('Number of upgrading T2 NAVAL '..self.Factories.NAVAL[2].UpgradingCount)
             LOG('Number of HQs T2 Naval '..self.Factories.NAVAL[2].HQCount)
             LOG('Number of HQs T3 Naval '..self.Factories.NAVAL[3].HQCount)
+            coroutine.yield(30)
+        end
+    end,
 
-            coroutine.yield(50)
+    UpgradeFactoryRNG = function(self, unit, type, tier)
+        local ALLBPS = __blueprints
+        upgradeID = ALLBPS[unit.UnitId].General.UpgradesTo
+        if not upgradeID then
+            WARN('No upgrade ID in blueprint for factory upgrade, aborting upgrade')
+            coroutine.yield(20)
+            return
+        end
+        if upgradeID then
+            if ALLBPS[unit.UnitId].CategoriesHash.LAND then
+                if ALLBPS[unit.UnitId].CategoriesHash.TECH1 then
+                    if self.Factories.LAND[2].HQCount > 0 then
+                        upgradeID = self.SupportUpgradeTable.LAND.T2[unit.UnitId]
+                    end
+                elseif ALLBPS[unit.UnitId].CategoriesHash.TECH2 then
+                    if self.Factories.LAND[3].HQCount > 0 then
+                        upgradeID = self.SupportUpgradeTable.LAND.T3[unit.UnitId]
+                    end
+                end
+            elseif ALLBPS[unit.UnitId].CategoriesHash.AIR then
+                if ALLBPS[unit.UnitId].CategoriesHash.TECH1 then
+                    if self.Factories.AIR[2].HQCount > 0 then
+                        upgradeID = self.SupportUpgradeTable.AIR.T2[unit.UnitId]
+                    end
+                elseif ALLBPS[unit.UnitId].CategoriesHash.TECH2 then
+                    if self.Factories.AIR[3].HQCount > 0 then
+                        upgradeID = self.SupportUpgradeTable.AIR.T3[unit.UnitId]
+                    end
+                end
+            elseif ALLBPS[unit.UnitId].CategoriesHash.AIR then
+                if ALLBPS[unit.UnitId].CategoriesHash.TECH1 then
+                    if self.Factories.NAVAL[2].HQCount > 0 then
+                        upgradeID = self.SupportUpgradeTable.AIR.T2[unit.UnitId]
+                    end
+                elseif ALLBPS[unit.UnitId].CategoriesHash.TECH2 then
+                    if self.Factories.NAVAL[3].HQCount > 0 then
+                        upgradeID = self.SupportUpgradeTable.AIR.T3[unit.UnitId]
+                    end
+                end
+            end
+            if not upgradeID then
+                upgradeID = ALLBPS[unit.UnitId].General.UpgradesTo
+            end
+        end
+        if upgradeID then
+            IssueUpgrade({unit}, upgradeID)
         end
     end,
 }
