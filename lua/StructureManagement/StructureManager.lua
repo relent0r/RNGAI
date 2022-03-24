7a5e7e03997478a101764be09b933d5111a2addc
Aaron Warner <aaron.warner@revera.co.nz>
2022-03-24 15:49:53 +1300

    add temp table for data capture.

--- lua/StructureManagement/StructureManager.lua
+++ lua/StructureManagement/StructureManager.lua
@@ -93,6 +93,30 @@ StructureManager = Class {
         coroutine.yield(Random(5,20))
         local ALLBPS = __blueprints
         while true do
+            -- Create a table rather than a million locals
+            local FactoryData = {
+                T2LANDHQCount = 0,
+                T3LANDHQCount = 0,
+                T2AIRHQCount = 0,
+                T3AIRHQCount = 0,
+                T2NAVALHQCount = 0,
+                T3NAVALHQCount = 0,
+                T1LANDUpgrading = 0,
+                T2LANDUpgrading = 0,
+                T1AIRUpgrading = 0,
+                T2AIRUpgrading = 0,
+                T1NAVALUpgrading = 0,
+                T2NAVALUpgrading = 0,
+                T1LAND = {},
+                T2LAND = {},
+                T3LAND = {},
+                T1AIR = {},
+                T2AIR = {},
+                T3AIR = {},
+                T1NAVAL = {},
+                T2NAVAL = {},
+                T3NAVAL = {},
+            }
             for k, manager in self.Brain.BuilderManagers do
                 if RNGGETN(manager.FactoryManager.FactoryList) > 0 then
                     for c, unit in manager.FactoryManager.FactoryList do
@@ -101,41 +125,40 @@ StructureManager = Class {
                                 if ALLBPS[unit.UnitId].CategoriesHash.TECH1 then
                                     RNGINSERT(self.Factories.LAND[1].Units, unit)
                                     if unit:IsUnitState('Upgrading') then
-                                        self.Factories.LAND[1].UpgradingCount = self.Factories.LAND[1].UpgradingCount + 1
+                                        FactoryData.T1LANDUpgrading = FactoryData.T1LANDUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH2 then
                                     RNGINSERT(self.Factories.LAND[2].Units, unit)
                                     if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
-                                        self.Factories.LAND[2].HQCount = self.Factories.LAND[2].HQCount + 1
+                                        FactoryData.T2LANDHQCount = FactoryData.T2LANDHQCount + 1
                                     end
                                     if unit:IsUnitState('Upgrading') then
-                                        self.Factories.LAND[2].UpgradingCount = self.Factories.LAND[2].UpgradingCount + 1
+                                        FactoryData.T2LANDUpgrading = FactoryData.T2LANDUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH3 then
                                     RNGINSERT(self.Factories.LAND[3].Units, unit)
                                     if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
-                                        self.Factories.LAND[3].HQCount = self.Factories.LAND[3].HQCount + 1
+                                        FactoryData.T3LANDHQCount = FactoryData.T3LANDHQCount + 1
                                     end
-                                    self.Factories.LAND[2].UpgradingCount = self.Factories.LAND[2].UpgradingCount + 1
                                 end
                             elseif ALLBPS[unit.UnitId].CategoriesHash.AIR then
                                 if ALLBPS[unit.UnitId].CategoriesHash.TECH1 then
                                     RNGINSERT(self.Factories.AIR[1].Units, unit)
                                     if unit:IsUnitState('Upgrading') then
-                                        self.Factories.AIR[1].UpgradingCount = self.Factories.AIR[1].UpgradingCount + 1
+                                        FactoryData.T1AIRUpgrading = FactoryData.T1AIRUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH2 then
                                     RNGINSERT(self.Factories.AIR[2].Units, unit)
                                     if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
-                                        self.Factories.AIR[2].HQCount = self.Factories.AIR[2].HQCount + 1
+                                        FactoryData.T2AIRHQCount = FactoryData.T2AIRHQCount + 1
                                     end
                                     if unit:IsUnitState('Upgrading') then
-                                        self.Factories.AIR[2].UpgradingCount = self.Factories.AIR[2].UpgradingCount + 1
+                                        FactoryData.T2AIRUpgrading = FactoryData.T2AIRUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH3 then
                                     RNGINSERT(self.Factories.AIR[3].Units, unit)
                                     if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
-                                        self.Factories.AIR[3].HQCount = self.Factories.AIR[3].HQCount + 1
+                                        FactoryData.T3AIRHQCount = FactoryData.T3AIRHQCount + 1
                                     end
                                 end
 
@@ -143,28 +166,31 @@ StructureManager = Class {
                                 if ALLBPS[unit.UnitId].CategoriesHash.TECH1 then
                                     RNGINSERT(self.Factories.NAVAL[1].Units, 1, unit)
                                     if unit:IsUnitState('Upgrading') then
-                                        self.Factories.NAVAL[1].UpgradingCount = self.Factories.NAVAL[1].UpgradingCount + 1
+                                        FactoryData.T1NAVALUpgrading = FactoryData.T1NAVALUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH2 then
                                     RNGINSERT(self.Factories.NAVAL[2].Units, unit)
                                     if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
-                                        self.Factories.NAVAL[2].HQCount = self.Factories.NAVAL[2].HQCount + 1
+                                        FactoryData.T2NAVALHQCount = FactoryData.T2NAVALHQCount + 1
                                     end
                                     if unit:IsUnitState('Upgrading') then
-                                        self.Factories.NAVAL[2].UpgradingCount = self.Factories.NAVAL[2].UpgradingCount + 1
+                                        FactoryData.T2NAVALUpgrading = FactoryData.T2NAVALUpgrading + 1
                                     end
                                 elseif ALLBPS[unit.UnitId].CategoriesHash.TECH3 then
                                     RNGINSERT(self.Factories.NAVAL[3].Units, unit)
                                     if not ALLBPS[unit.UnitId].CategoriesHash.SUPPORT then
-                                        self.Factories.NAVAL[3].HQCount = self.Factories.NAVAL[3].HQCount + 1
+                                        FactoryData.T3NAVALHQCount = FactoryData.T3NAVALHQCount + 1
                                     end
-                                    self.Factories.NAVAL[3].UpgradingCount = self.Factories.NAVAL[3].UpgradingCount + 1
+                                    FactoryData.T3NAVALUpgrading = FactoryData.T3NAVALUpgrading + 1
                                 end
                             end
                         end
                     end
                 end
             end
+            self.Factories.LAND[1].UpgradingCount
+            self.Factories.LAND[2].UpgradingCount
+            self.Factories.LAND[2].HQCount
             LOG('Structure Manager')
             LOG('Number of upgrading T1 Land '..self.Factories.LAND[1].UpgradingCount)
             LOG('Number of upgrading T2 Land '..self.Factories.LAND[2].UpgradingCount)
