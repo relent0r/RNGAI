631878864f8378659fa0dacdcef72ac420ad06b6
Aaron Warner <aaron.warner@revera.co.nz>
2023-08-07 11:14:53 +1200

    add option for replacement acu movement position.

--- lua/AI/RNGUtilities.lua
+++ lua/AI/RNGUtilities.lua
@@ -5577,3 +5577,52 @@ function GenerateChokePointLines(aiBrain)
 
 end
 
+
+--[[
+    -- Calculate dot product between two 3D vectors (same as before)
+
+-- Algorithm function to calculate the second move position and determine left/right
+function calculate_second_move_position(lerped_position, enemy_position, offset_distance, is_on_right)
+    -- Step 1: Calculate the direction vector from the friendly unit to the enemy unit.
+    local direction_vector = {
+        x = enemy_position.x - lerped_position.x,
+        y = enemy_position.y - lerped_position.y,
+        z = enemy_position.z - lerped_position.z
+    }
+
+    -- Step 2: Find a perpendicular vector to the direction vector.
+    local perpendicular_vector = {
+        x = -direction_vector.z,
+        y = direction_vector.y,
+        z = direction_vector.x
+    }
+
+    -- Step 3: Normalize the perpendicular vector.
+    local perpendicular_magnitude = math.sqrt(perpendicular_vector.x * perpendicular_vector.x + perpendicular_vector.y * perpendicular_vector.y + perpendicular_vector.z * perpendicular_vector.z)
+    if perpendicular_magnitude > 0 then
+        perpendicular_vector.x = perpendicular_vector.x / perpendicular_magnitude
+        perpendicular_vector.y = perpendicular_vector.y / perpendicular_magnitude
+        perpendicular_vector.z = perpendicular_vector.z / perpendicular_magnitude
+    end
+
+    -- Step 4: Multiply the normalized perpendicular vector by the fixed offset_distance.
+    local sign = is_on_right and 1 or -1 -- Use 1 for right, -1 for left
+    local second_move_position = {
+        x = lerped_position.x + sign * perpendicular_vector.x * offset_distance,
+        y = lerped_position.y + sign * perpendicular_vector.y * offset_distance,
+        z = lerped_position.z + sign * perpendicular_vector.z * offset_distance
+    }
+
+    return second_move_position
+end
+
+-- Example usage
+local lerped_position = { x = 10, y = 0, z = 5 }
+local enemy_position = { x = 15, y = 0, z = 10 }
+local offset_distance = 2 -- Fixed distance of 2 units from the lerped position
+local is_on_right = true -- Set this to true for the second move position to be on the right side, false for left side
+
+local second_move_position = calculate_second_move_position(lerped_position, enemy_position, offset_distance, is_on_right)
+print(second_move_position.x, second_move_position.y, second_move_position.z)
+
+]]
