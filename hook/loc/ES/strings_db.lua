30a424b8eab63d444515e8727a2ef1fb99360747
Aaron Warner <aaron.warner@concepts.co.nz>
2024-05-07 08:16:41 +1200

    readd loc files

---
+++ hook/loc/ES/strings_db.lua
@@ -0,0 +1,13 @@
+﻿
+RNGAI_0001="AI : RNGAI Standard"
+RNGAI_0002="Good for 10km or smaller maps."
+RNGAI_0003="AIx: RNGAI Standard"
+RNGAI_0004="Good for 10km or smaller maps."
+
+-- Translation strings for your AI lobby options.
+aisettingsRNG_0200="Enable Threat Debug"
+aisettingsRNG_0201="Display live threat updates when a player is focused on"
+aisettingsRNG_0202="On"
+aisettingsRNG_0203="help text on"
+aisettingsRNG_0204="Off"
+aisettingsRNG_0205="help text off"
---
+++ hook/loc/RU/strings_db.lua
@@ -0,0 +1,13 @@
+﻿
+RNGAI_0001="AI : RNGAI Standard"
+RNGAI_0002="Good for 10km or smaller maps."
+RNGAI_0003="AIx: RNGAI Standard"
+RNGAI_0004="Good for 10km or smaller maps."
+
+-- Translation strings for your AI lobby options.
+aisettings_0200="Enable Threat Debug"
+aisettings_0201="Display live threat updates when a player is focused on"
+aisettings_0202="On"
+aisettings_0203="help text on"
+aisettings_0204="Off"
+aisettings_0205="help text off"
