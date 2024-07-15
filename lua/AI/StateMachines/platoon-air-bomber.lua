501dc0bdd3063f310546ce31b756cd66b3de88b5
Aaron Warner <aaron.warner@concepts.co.nz>
2024-07-16 11:47:28 +1200

    remove logs

--- lua/AI/StateMachines/platoon-air-bomber.lua
+++ lua/AI/StateMachines/platoon-air-bomber.lua
@@ -136,6 +136,7 @@ AIPlatoonBomberBehavior = Class(AIPlatoonRNG) {
                 local target, countRequired , acuIndex, strikeDamage = RUtils.CheckACUSnipe(aiBrain, 'Air')
                 if target then
                     --LOG('ACU Snipe found for bombers, strike damage required is '..tostring(strikeDamage))
+                    --LOG('Bomber has acu via ACUSnipe')
                     local enemyAcuHealth = aiBrain.EnemyIntel.ACU[acuIndex].HP
                     if self.PlatoonStrikeDamage > enemyAcuHealth * 0.80 or enemyAcuHealth < 2500 then
                         self.BuilderData = {
